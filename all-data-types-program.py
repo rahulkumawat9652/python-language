04fe52313df6cc0036a391cd873dac734e538bef
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-04-18 21:58:34 +0530

    all data types program

---
+++ all-data-types-program.py
@@ -0,0 +1,21 @@
+x = 55
+print(x)
+print(type(x))
+y = 44.5
+print(y)
+print(type(y))
+name = "rahul"
+print(name)
+print(type(name))
+adult = False
+print(adult)
+print(type(adult))
+marks = [12,345,456,56]
+print(marks)
+print(type(marks))
+marks = (12,345,456,56)
+print(marks)
+print(type(marks))
+marks = {"rahul": 12, "rohit": 345,"shiv": 456}
+print(marks)
+print(type(marks))
