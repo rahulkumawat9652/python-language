02bd8cb06dee9fed5d3e9a79d029ea586e1615e6
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-04-19 20:40:25 +0530

    assignment coparsion operator program

---
+++ assignment-comparsion-operator-program.py
@@ -0,0 +1,17 @@
+a=4
+b=5
+# arithmetic operators
+print("value of a+b is ",a+b)
+print("value of a-b is ",a-b)
+print("value of a*b is ",a*b)
+print("value of a/b is ",a/b)
+print("value of a%b is ",a%b)
+print("value of a//b is ",a//b)
+print("value of a**b is ",a**b)
+# Comparison operators
+print("value of a==b is ",a==b)
+print("value of a!=b is ",a!=b)
+print("value of a>=b is ",a>=b)
+print("value of a<=b is ",a<=b)
+print("value of a>b is ",a>b)
+print("value of a<b is ",a<b)
