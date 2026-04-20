97d06300bf1eb0286c2ebaf91409b9f1197f698b
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-04-20 20:22:30 +0530

    average three number

---
+++ average-three-number.py
@@ -0,0 +1,5 @@
+num1 = int(input("enter number 1 "))
+num2 = int(input("enter number 2 "))
+num3 = int(input("enter number 3 "))
+average = (num1 + num2 + num3)/3
+print("average of three number is",average)
