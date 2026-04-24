df9408cd7d4809578fe083a320ea06d78539894d
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-04-24 19:51:08 +0530

    first number divisibal by second condition program

---
+++ first-number-divisibal-by-second-condition-program.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter first number 1 "))
+num2 = int(input("enter second number 2 "))
+if num1%num2==0:
+    print("first number is divisible by second number")
+else:
+    print("first number is not divisible by second number")
