d267aff4e594fa0c012588180a792ebb71e846ab
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-04-24 22:19:10 +0530

    print veriable

---
+++ print-veriable.py
@@ -0,0 +1,13 @@
+name = "rahul"
+age = 19
+gender = 'male'
+# method -1
+print("My name is ",name)
+print("My age is ",age)
+print("My gender is ",gender)
+# method -2
+print(f"My name is {name}")
+print(f"My age is {age}")
+print(f"My gender is {gender}")
+# method -3
+print("My name is",name,"My age is",age,"My gender is",gender)
