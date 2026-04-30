599b3d1e14425a3d64d1b56d370a6ca299f5629a
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-04-30 10:56:03 +0530

    string palindrome program

--- string-palindrome-program.py
+++ string-palindrome-program.py
@@ -1,5 +1,5 @@
-list = input("enter string ")
-if list == list[ : : -1]:
+my_list = input("enter string ")
+if my_list == my_list[ : : -1]:
     print("it is a palindrome")
 else:
     print("it is not a palindrome")
