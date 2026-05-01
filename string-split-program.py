5a3ef173c35979b536507fc1822e484d4aa7df4e
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-05-01 11:16:43 +0530

    string split program

---
+++ string-split-program.py
@@ -0,0 +1,9 @@
+a = "Hello world this is python"
+words = a.split()
+print(words)
+a = "Hello-world-this-is-python"
+words = a.split()
+print(words)
+a = "Hello world this is python"
+words = a.split("-")
+print(words)
