5b5e0e0267fc08864f8b0aa89ac3db4b2f28c4d0
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-05-01 13:45:21 +0530

    set mathod base program

---
+++ set-mathod-base-program.py
@@ -0,0 +1,10 @@
+my_set1 = {23,45,67,89}
+my_set2 = {45,67,78,90}
+print(my_set1)
+print(my_set2)
+print(my_set1.union(my_set2))
+print(my_set1.intersection(my_set2))
+my_set1.add(100)
+print(my_set1)
+my_set2.remove(90)
+print(my_set2)
