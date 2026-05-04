d057fb4da11865985306f9de10ee652369532784
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-05-04 20:23:54 +0530

    iterating kay value in dictionary program

---
+++ iterating-kay-value-in-dictionary-program.py
@@ -0,0 +1,9 @@
+my_dict = {"name":"rahul","age":20,"gender":"male"}
+for i in my_dict.keys():
+    print(i)
+for i in my_dict.keys():
+    print(my_dict[i])
+for i in my_dict.values():
+    print(i)
+for k,v in my_dict.items():
+    print(f"{k} -> {v}")
